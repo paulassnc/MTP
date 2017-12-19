0a47284be57a7026712a8687429e028df7969d9c
paulassnc <31476120+paulassnc@users.noreply.github.com>
2017-12-19 19:31:40 -0200

    Update vet1.c

--- aula20170927/vet1.c
+++ aula20170927/vet1.c
@@ -1,29 +1,23 @@
 #include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 #define N 10000
- int main() {
+int main(){
     srand(time(0));
-    int numeros[N];
-    int i,maximo;
-    float media = 0.f;
-    printf("Entre com o numero maximo a ser sorteado:");
-    scanf("%d", &maximo);
-    for(i=0; i<N; i++)
-    numeros[i] = rand()%(maximo+1);
-    //Media
-    for(i=0; i<N;i++)
-        media += numeros[i];
+    int vet[N];
+    int n, i;
+    float media=0.f;
+    printf("entre com o numero maximo a ser sorteado:");
+    scanf("%d", &n);
+    for(i=0; i<N; i++){
+        vet[i] = rand()%(n+1);
+    }
+    for(i=0; i<N; i++){
+        media += vet[i];
+    }
     media /= N;
-    printf("A media dos %d numeros: %f\n", N , media);
-
-
-
-
+    printf("a media e': %f", media);
 
     return EXIT_SUCCESS;
-
-
-    }
-
-
+}

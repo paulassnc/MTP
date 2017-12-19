5b5e5f2657267d9ddff231d7083f5e7fb05c1c73
paulassnc <31476120+paulassnc@users.noreply.github.com>
2017-12-19 19:42:26 -0200

    Create din2.c

---
+++ aula20171101/din2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void preenche(float * elemento);
+void imprime(float **matriz, int N, int M);
+
+int main() {
+  int i, j;
+  int N, M;
+  float **matriz;
+  printf("Entre com a ordem da matriz, no formato 'NxM': ");
+  scanf("%dx%d", &N, &M);
+  matriz = calloc(N,sizeof(float*));
+  for(i = 0; i < N; i++) {
+	matriz[i] = calloc(M,sizeof(float));
+    for(j = 0; j < M; j++) {
+      printf("Elemento (%d,%d): ", i, j);
+      preenche(&matriz[i][j]);
+    }
+  }
+  imprime(matriz, N, M);
+  for(i = 0; i < N; i++)
+	free(matriz[i]);
+  free(matriz);
+  return 0;
+}
+
+void preenche(float * elemento) {
+  scanf("%f", elemento);
+}
+
+void imprime(float **matriz, int N, int M) {
+  int i, j;
+  printf("matriz %dx%d:\n", N, M);
+  for(i = 0; i < N; i++)
+    for(j = 0; j < M; j++)
+      printf("%g%c", matriz[i][j], (j == M-1)? '\n':'\t');
+}

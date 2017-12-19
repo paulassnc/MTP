34683f35666585aeac96432fabe3e36f4bb95f06
paulassnc <31476120+paulassnc@users.noreply.github.com>
2017-12-19 19:15:38 -0200

    Update mat1.c

--- aula20170920/mat1.c
+++ aula20170920/mat1.c
@@ -2,12 +2,13 @@
 #include<stdlib.h>
 #include<math.h>
 int main(){
-   float x1,x2,y1,y2,distancia;
-   printf("Entre com as coordenadas x1,y1:");
-   scanf("%f,%f", &x1,&y1);
-   printf("Entre com as coordenadas x2,y2:");
-   scanf("%f,%f",&x2, &y2);
-   distancia = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
-   printf("A distancia entre os pontos = %f\n", distancia);
-   return EXIT_SUCCESS;
-   }
+    float x1, x2, y1, y2, distancia;
+    printf("entre com x1, y1: ");
+    scanf("%f,%f", &x1, &y1);
+    printf("entre com x2, y2: ");
+    scanf("%f,%f", &x2, &y2);
+    distancia= sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+    printf("a distancia e': %f\n", distancia);
+
+    return EXIT_SUCCESS;
+}

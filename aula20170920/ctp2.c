781cc89406cea5e685fd901259b4a9fac337d9f1
paulassnc <31476120+paulassnc@users.noreply.github.com>
2017-12-19 19:19:33 -0200

    Update ctp2.c

--- aula20170920/ctp2.c
+++ aula20170920/ctp2.c
@@ -1,24 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
-#define N 256
-int main() {
-    char frase[N];
-    int i;
-    printf("Entre com uma frase:\n");
-    fgets(frase, N, stdin);
-    for(i = 0; frase[i]; i++){
-            frase[i] = tolower(frase[i]);
-
+#define n 256
+int main(){
+    char frase[n];
+    int i=0;
+    printf("Digite uma frase:\n");
+    fgets(frase,n,stdin);
+    for (i=0; frase[i]; i++){
+        frase[i]= tolower(frase[i]);
     }
     printf("%s", frase);
 
-
-
     return EXIT_SUCCESS;
-
-
-
-
-
 }

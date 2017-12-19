f8518fb17c077eae3b303564ff22220b304caa00
paulassnc <31476120+paulassnc@users.noreply.github.com>
2017-12-19 19:17:20 -0200

    Update iif1.c

--- aula20170920/iif1.c
+++ aula20170920/iif1.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-int main() {
+int main(){
     srand(time(0));
     float probuser;
-    int i, contagem=0;
-    printf("Entre com uma probabilidade[0.0~1.0]:");
+    int i=0, cont=0;
+    printf("entre com uma probabilidade[0.0~1.0: ");
     scanf("%f", &probuser);
-    for(i=0; i<100000; i++){
-    if((float)rand()/(float)RAND_MAX< probuser)
-    contagem++;
-    }
-    printf("Contagem em 1000:%d\n", contagem);
+    for(i=0; i < 1000; i++)
+        if((float)rand()/(float)RAND_MAX < probuser)
+            cont++;
+    printf("contagem em 1000: %d\n", cont);
     return EXIT_SUCCESS;
-
-
-
-
 }

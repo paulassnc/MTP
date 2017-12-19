c2dde29c4ffaf4b2c2dbe544e3f1a080caf56fc8
paulassnc <31476120+paulassnc@users.noreply.github.com>
2017-12-19 19:40:59 -0200

    Update din1.c

--- aula20171101/din1.c
+++ aula20171101/din1.c
@@ -1,36 +1,35 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-float media(float * numeros, int qtde){
-float m = 0.0f;
-int i;
-for(i = 0; i < qtde; i++) m += numeros[i];
-m /= qtde;
-return m;
-}
-float desviopadrao(float * numeros, int qtde){
-    float m = media(numeros, qtde);
-    float dp = 0.0f;
+#include<math.h>
+#include<stdlib.h>
+
+float media(float*numeros, int qtd){
+    float m= 0.0f;
     int i;
-    for(i = 0; i < qtde; i++) dp += pow(numeros[i]- m,2);
-    dp/=qtde;
+    for(i=0; i<qtd; i++) m += numeros[i];
+    m /= qtd;
+    return m;
+}
+float desviopadrao(float *numeros, int qnt){
+    float m= media(numeros,qnt);
+    float dp= 0.0f;
+    int i;
+    for(i=0; i<qnt; i++) dp += pow(numeros[i]-m,2);
+    dp /= qnt;
     return sqrt(dp);
 }
-int main () {
-    int qtde, i;
-    float * numeros;
-    printf("Quantos numeros voce precisa?");
-    scanf("%d", &qtde);
-    numeros = (float*) malloc(qtde*sizeof(float));
-    for(i=0; i < qtde; i++){
-        printf("Entre com o %do numero:", i+1);
+
+int main() {
+    int qnt, i;
+    float *numeros;
+    printf("quantos numeros ?");
+    scanf("%d", &qnt);
+    numeros = (float*)malloc(qnt*sizeof(float));
+    for(i=0; i<qnt; i++){
+        printf("entre com um %do numero: ", i+1);
         scanf("%f", numeros+i);
-        }
-        printf("A media dos numeros e: %f\n", media(numeros, qtde));
-        printf("O desvio-padrao dos numeros e: %f\n", desviopadrao(numeros,qtde));
-
-
+    }
+    printf("a media e: %f\n", media(numeros, qnt));
+    printf("desvio padrao e %f\n", desviopadrao(numeros, qnt));
     free(numeros);
     return 0;
-
 }
